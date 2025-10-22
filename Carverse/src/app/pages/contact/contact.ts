90d045c6f8b9a73787d5161e3c5194df50e63a88
Mazen-Mohammed <mazen.muhamed09@gmail.com>
2025-10-22 18:08:01 +0300

    Contactpage

--- Carverse/src/app/pages/contact/contact.ts
+++ Carverse/src/app/pages/contact/contact.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import { FormsModule } from '@angular/forms';
+
+interface ContactForm {
+  name: string;
+  email: string;
+  phone: string;
+  subject: string;
+  message: string;
+}
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
-export class Contact {
 
+export class Contact {
+  form: ContactForm = {
+    name: '',
+    email: '',
+    phone: '',
+    subject: '',
+    message: '',
+  };
+
+  public sendEmail() {
+
+    emailjs
+      .send('service_x4wiueo', 'template_wstjog1', {...this.form}, {
+        publicKey: 'O_zIqaAzvvJnnPhhq',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
 }
