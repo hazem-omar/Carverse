21a8b35b510f685661a386e5270a2475c4cad242
Mazen-Mohammed <mazen.muhamed09@gmail.com>
2025-11-11 00:37:51 +0200

    Update app structure: replace router outlet with app-home and add Featured component

--- Carverse/src/app/app.ts
+++ Carverse/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Cover } from './pages/cover/cover';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
 import { Payment } from './pages/payment/payment';
 import { Wishlist } from './pages/wishlist/wishlist';
 import { Register } from './pages/register/register';
 import { Login } from './pages/login/login';
+import { Featured } from './pages/featured/featured';
 import { CarProduct } from './pages/car-product/car-product';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
@@ -14,7 +16,7 @@ import { Cart } from './pages/cart/cart';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Footer,About,CarProduct,Cart,Contact,Home,Login,Payment,Register,Wishlist],
+  imports: [RouterOutlet,Navbar,Footer,About,CarProduct,Cart,Contact,Home,Login,Payment,Register,Wishlist,Cover,Featured],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
