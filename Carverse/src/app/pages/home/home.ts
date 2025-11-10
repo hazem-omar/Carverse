738bd109f84a6c85bda3d1072165d176ab9bce55
Mazen-Mohammed <mazen.muhamed09@gmail.com>
2025-11-11 00:37:38 +0200

    Refactor home page: remove carousel and integrate component structure

--- Carverse/src/app/pages/home/home.ts
+++ Carverse/src/app/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-
+import { Cover } from '../cover/cover';
+import { About } from '../about/about';
+import { Contact } from '../contact/contact';
+import { Featured } from '../featured/featured';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Cover,About,Contact,Featured],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
